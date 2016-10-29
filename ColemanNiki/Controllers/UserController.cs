b5a63db31fa9f3642f5cc5ae12a47e7c591afb24
ColemanNiki <ColemanNiki@COLEMANNIKI>
2016-10-29 11:39:48 +0800

    数据库尝试

--- ColemanNiki/Controllers/UserController.cs
+++ ColemanNiki/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text;
 using ColemanNiki.Models.DataBase;
 
 namespace ColemanNiki.Controllers
@@ -15,6 +16,13 @@ namespace ColemanNiki.Controllers
         
         public ActionResult Index()
         {
+            dream temp = new dream();
+            temp.userId=1;
+            temp.remark = Encoding.Default.GetBytes("f");
+            temp.state = 1;
+            temp.title = Encoding.Default.GetBytes("f");
+            db.dreams.Add(temp);
+            db.SaveChanges();
             return View();
         }
 
